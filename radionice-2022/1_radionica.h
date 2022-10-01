1691bc2e24ddb78d8f7e9315cec43cebbf665090
Andro-Robotika <114672642+Andro-Robotika@users.noreply.github.com>
2022-10-01 09:47:23 +0200

    Update 1_radionica.h

--- radionice-2022/1_radionica.h
+++ radionice-2022/1_radionica.h
@@ -3,4 +3,27 @@
 
 void RobotLine::radionica() {
 
+
+display("ANDRO");
+
+// ZADATAK: Neka robot opiše četverokut.
+go(60, 60);
+delayMs(3000);
+go(-90, 90);
+delayMs(500);
+go(60, 60);
+delayMs(3000);
+go(-90, 90);
+delayMs(500);
+go(60, 60);
+delayMs(3000);
+go(-90, 90);
+delayMs(500);
+go(60, 60);
+delayMs(3000);
+go(-90, 90);
+delayMs(500);
+stop(), end();
+
+
 }
